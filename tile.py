ae71e2b6af0b76cb93a9d844fd3a1d0c7be662b9
YBN 140 <ybndns140@gmail.com>
2024-07-09 23:25:47 +0200

    b

--- tile.py
+++ tile.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from PIL import Image
 import os
 
@@ -32,38 +31,3 @@ for y in range(num_tiles_y):
         tile.save(f"{tiles_directory}/{x}_{y}.png")
 
 print("Tiles created successfully.")
-=======
-from PIL import Image
-import os
-
-# Path to your image
-image_path = "C:/Users/mihaj/Desktop/YBNDrillin-gmap-main/YBNDrillin-gmap/newtiles/based/lc.jpg"
-
-# Load the image
-image = Image.open(image_path)
-
-# Define the number of tiles
-num_tiles_x = 32
-num_tiles_y = 32
-
-# Get image dimensions
-width, height = image.size
-
-# Calculate tile dimensions
-tile_width = width // num_tiles_x
-tile_height = height // num_tiles_y
-
-# Create a directory to save the tiles if it doesn't exist
-tiles_directory = "tiles_2x2"
-os.makedirs(tiles_directory, exist_ok=True)
-
-# Save each tile
-for y in range(num_tiles_y):
-    for x in range(num_tiles_x):
-        # Define the box for cropping
-        box = (x * tile_width, y * tile_height, (x + 1) * tile_width, (y + 1) * tile_height)
-        tile = image.crop(box)
-        tile.save(f"{tiles_directory}/{x}_{y}.png")
-
-print("Tiles created successfully.")
->>>>>>> 44ce945332f2586bcd807b71915769cf1142efce
